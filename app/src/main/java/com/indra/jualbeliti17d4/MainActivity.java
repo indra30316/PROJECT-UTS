4da280b9a1088a60cee64e47fcf6b3a9213a2dd8
fuadindraseptian <fuadindraseptian30@gmail.com>
2020-04-19 18:44:37 +0700

    no message

---
+++ app/src/main/java/com/indra/jualbeliti17d4/MainActivity.java
@@ -0,0 +1,14 @@
+package com.indra.jualbeliti17d4;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
